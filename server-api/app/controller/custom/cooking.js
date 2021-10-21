9d7134f476b767fc1f31c09a284e94986c1b317e
ZhouKaiDong <zhoukaidong@cangoonline.com>
2021-10-21 11:35:34 +0800

    优化

--- server-api/app/controller/custom/cooking.js
+++ server-api/app/controller/custom/cooking.js
@@ -14,7 +14,7 @@ class CookingController extends BaseController {
             pageSize: "pageSize 为必要参数"
         });
         let querySql = `select d.*,DATE_FORMAT(d.created_at,'%Y-%m-%d %H:%i') as created_at_ftt from cooking d where d.deleted='00' and d.publish='01'`;
-        let orderBy = `order by d.date desc,d.moment desc`
+        let orderBy = `order by d.create_at desc`
         let replacements = {};
         //拼接动态参数
         let result = await service.model.pageQuery(querySql, replacements, page, pageSize, orderBy);
@@ -126,7 +126,7 @@ class CookingController extends BaseController {
             pageSize: "pageSize 为必要参数"
         });
         let querySql = `select t.* from cuisine t where t.deleted='00'`;
-        let orderBy = `order by t.created_at desc`
+        let orderBy = `order by t.date desc,t.moment desc`
         let replacements = {};
         //拼接动态参数
         let result = await service.model.pageQuery(querySql, replacements, page, pageSize, orderBy);
